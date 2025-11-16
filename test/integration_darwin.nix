ca30f2a1df87c950a6118b56e55950e8e694fc68
Zebreus zebreus@zebre.us <zebreus@zebre.us>
2025-11-16 15:16:31 +0100

    Unify flake checks for different architectures

--- test/integration_darwin.nix
+++ test/integration_darwin.nix
@@ -1,30 +1,62 @@
 {
-  config,
-  pkgs,
-  options,
-  ...
+  darwin,
+  pkgs ? import <nixpkgs> {
+    inherit system;
+    config = { };
+  },
+  system ? builtins.currentSystem,
+  home-manager ? <home-manager>,
 }:
 let
-  secret = "hello";
-  testScript = pkgs.writeShellApplication {
-    name = "agenix-integration";
-    text = ''
-      grep "${secret}" "${config.age.secrets.system-secret.path}"
-    '';
-  };
+  integrate_me =
+    {
+      config,
+      pkgs,
+      options,
+      ...
+    }:
+    let
+      secret = "hello";
+      testScript = pkgs.writeShellApplication {
+        name = "agenix-integration";
+        text = ''
+          grep "${secret}" "${config.age.secrets.system-secret.path}"
+        '';
+      };
+    in
+    {
+      imports = [
+        ./install_ssh_host_keys_darwin.nix
+        ../modules/age.nix
+      ];
+
+      age = {
+        identityPaths = options.age.identityPaths.default ++ [ "/etc/ssh/this_key_wont_exist" ];
+        secrets.system-secret.file = ../example/secret1.age;
+      };
+
+      environment.systemPackages = [ testScript ];
+
+      system.stateVersion = 6;
+    };
 in
-{
-  imports = [
-    ./install_ssh_host_keys_darwin.nix
-    ../modules/age.nix
+(darwin.lib.darwinSystem {
+  inherit system;
+  modules = [
+    integrate_me
+
+    # Allow new-style nix commands in CI
+    { nix.extraOptions = "experimental-features = nix-command flakes"; }
+
+    home-manager.darwinModules.home-manager
+    {
+      home-manager = {
+        verbose = true;
+        useGlobalPkgs = true;
+        useUserPackages = true;
+        backupFileExtension = "hmbak";
+        users.runner = ./integration_hm_darwin.nix;
+      };
+    }
   ];
-
-  age = {
-    identityPaths = options.age.identityPaths.default ++ [ "/etc/ssh/this_key_wont_exist" ];
-    secrets.system-secret.file = ../example/secret1.age;
-  };
-
-  environment.systemPackages = [ testScript ];
-
-  system.stateVersion = 6;
-}
+}).system
