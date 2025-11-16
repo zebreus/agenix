e50c025a1b25f8cdc701ec2f1de9768f5047c700
Zebreus zebreus@zebre.us <zebreus@zebre.us>
2025-11-16 21:43:56 +0100

    Attempt to fix nix darwin activation

--- test/install_ssh_host_keys_darwin.nix
+++ test/install_ssh_host_keys_darwin.nix
@@ -1,17 +1,18 @@
 # Do not copy this! It is insecure. This is only okay because we are testing.
+{ config, ... }:
 {
-  system.activationScripts.extraUserActivation.text = ''
+  system.activationScripts.homemanager-test-activation.text = ''
     echo "Installing system SSH host key"
-    sudo cp ${./example_keys/system1.pub} /etc/ssh/ssh_host_ed25519_key.pub
-    sudo cp ${./example_keys/system1} /etc/ssh/ssh_host_ed25519_key
-    sudo chmod 644 /etc/ssh/ssh_host_ed25519_key.pub
-    sudo chmod 600 /etc/ssh/ssh_host_ed25519_key
+    sudo -u root cp ${./example_keys/system1.pub} /etc/ssh/ssh_host_ed25519_key.pub
+    sudo -u root cp ${./example_keys/system1} /etc/ssh/ssh_host_ed25519_key
+    sudo -u root chmod 644 /etc/ssh/ssh_host_ed25519_key.pub
+    sudo -u root chmod 600 /etc/ssh/ssh_host_ed25519_key
 
     echo "Installing user SSH host key"
-    mkdir -p "$HOME/.ssh"
-    cp ${./example_keys/user1.pub} "$HOME/.ssh/id_ed25519.pub"
-    cp ${./example_keys/user1} "$HOME/.ssh/id_ed25519"
-    chmod 644 "$HOME/.ssh/id_ed25519.pub"
-    chmod 600 "$HOME/.ssh/id_ed25519"
+    sudo -u ${config.system.primaryUser} mkdir -p "${config.system.primaryUserHome}/.ssh"
+    sudo -u ${config.system.primaryUser} cp ${./example_keys/user1.pub} "${config.system.primaryUserHome}/.ssh/id_ed25519.pub"
+    sudo -u ${config.system.primaryUser} cp ${./example_keys/user1} "${config.system.primaryUserHome}/.ssh/id_ed25519"
+    sudo -u ${config.system.primaryUser} chmod 644 "${config.system.primaryUserHome}/.ssh/id_ed25519.pub"
+    sudo -u ${config.system.primaryUser} chmod 600 "${config.system.primaryUserHome}/.ssh/id_ed25519"
   '';
 }
