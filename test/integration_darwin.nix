9342f0ca10d555c508c9dee60a0359d3f85e10f5
Zebreus zebreus@zebre.us <zebreus@zebre.us>
2025-11-16 15:36:27 +0100

    Start cleaning up darwin tests

--- test/integration_darwin.nix
+++ test/integration_darwin.nix
@@ -7,56 +7,53 @@
   system ? builtins.currentSystem,
   home-manager ? <home-manager>,
 }:
-let
-  integrate_me =
-    {
-      config,
-      pkgs,
-      options,
-      ...
-    }:
-    let
-      secret = "hello";
-      testScript = pkgs.writeShellApplication {
-        name = "agenix-integration";
-        text = ''
-          grep "${secret}" "${config.age.secrets.system-secret.path}"
-        '';
-      };
-    in
-    {
-      imports = [
-        ./install_ssh_host_keys_darwin.nix
-        ../modules/age.nix
-      ];
-
-      age = {
-        identityPaths = options.age.identityPaths.default ++ [ "/etc/ssh/this_key_wont_exist" ];
-        secrets.system-secret.file = ../example/secret1.age;
-      };
-
-      environment.systemPackages = [ testScript ];
-
-      system.stateVersion = 6;
-    };
-in
 (darwin.lib.darwinSystem {
   inherit system;
   modules = [
-    integrate_me
+    (
+      {
+        config,
+        pkgs,
+        options,
+        ...
+      }:
+      let
+        secret = "hello";
+        testScript = pkgs.writeShellApplication {
+          name = "agenix-integration";
+          text = ''
+            grep "${secret}" "${config.age.secrets.system-secret.path}"
+          '';
+        };
+      in
+      {
+        imports = [
+          ./install_ssh_host_keys_darwin.nix
+          ../modules/age.nix
 
-    # Allow new-style nix commands in CI
-    { nix.extraOptions = "experimental-features = nix-command flakes"; }
+          (home-manager.darwinModules.home-manager {
+            home-manager = {
+              verbose = true;
+              useGlobalPkgs = true;
+              useUserPackages = true;
+              backupFileExtension = "hmbak";
+              users.runner = ./integration_hm_darwin.nix;
+            };
+          })
+        ];
 
-    home-manager.darwinModules.home-manager
-    {
-      home-manager = {
-        verbose = true;
-        useGlobalPkgs = true;
-        useUserPackages = true;
-        backupFileExtension = "hmbak";
-        users.runner = ./integration_hm_darwin.nix;
-      };
-    }
+        age = {
+          identityPaths = options.age.identityPaths.default ++ [ "/etc/ssh/this_key_wont_exist" ];
+          secrets.system-secret.file = ../example/secret1.age;
+        };
+
+        environment.systemPackages = [ testScript ];
+
+        # Allow new-style nix commands in CI
+        nix.extraOptions = "experimental-features = nix-command flakes";
+
+        system.stateVersion = 6;
+      }
+    )
   ];
 }).system
